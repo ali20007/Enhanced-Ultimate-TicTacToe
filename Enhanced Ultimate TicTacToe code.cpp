5c04f9401e3250eb5446cbe7b7214f33d991303d
Omar Kamal Ibrahim <104209197+omar-kamal8@users.noreply.github.com>
2022-05-01 15:35:58 +0200

    Update Enhanced Ultimate TicTacToe code.cpp

--- Enhanced Ultimate TicTacToe code.cpp
+++ Enhanced Ultimate TicTacToe code.cpp
@@ -1,8 +1,9 @@
-/*Enhanced Ultimate TicTacToe Game by Omar Kamal ibrahim -- Omar Mohamed Mahmoud -- Ali Ashraf Ali
-Game Description: It is the Normal X-O game but with 9 boards. In order to win, you have to complete 3 boards as rows or as columns or as diagonals.
-Rules: You will first choose a board and a cell to start with, the cell you play at is the same number of the board the next player will play at and the
-cell that he chooses will be the board  will play at etc.
-It has two modes of playing, 1)Against the computer or 2)Against A friend.
+/* The Definition by a Linked List
+struct board
+{
+    bool X, O, empty;
+    board* next;
+};
 */
 #include <iostream>
 #include <sstream>
